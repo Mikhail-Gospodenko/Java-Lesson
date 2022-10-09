f88b223575ae7b32cfbbb45e1dc4f0d949d1d054
Mikhail-Gospodenko <mgospodenko@gmail.com>
2022-10-09 20:18:03 +1000

    rework1

--- src/test/java/work1.java
+++ src/test/java/work1.java
@@ -1,32 +1,25 @@
 public class work1 {
     public static void main(String[] args) {
-        calculate();
-        trueOrFalse();
-        negativity();
-        positivity();
-        negativity();
-        greeting();
-        leaping();
+        calculate(123, 13,167,12);
+        trueOrFalse(12,21);
+        positivity(-5);
+        negativity(-2);
+        greeting("Михаил");
+        leaping(2014);
 
 
     }
 
     // задание 3
-    static double calculate() {
-        int a, b, c, d, result;
-        a = 123;
-        b = 672;
-        c = 1290;
-        d = 12;
+    static double calculate(int a,int b,int c, int d) {
+        int  result;
         result = a * (b + (c / d));
         System.out.println("Answer is " + result);
         return result;
     }
 
     // задание 4
-    static boolean trueOrFalse() {
-        int number1 = 12;
-        int number2 = 21;
+    static boolean trueOrFalse(int number1, int number2) {
         int resultOfPlus = number1 + number2;
         if ((resultOfPlus >= 10 && resultOfPlus <= 20)) {
             return true;
@@ -36,8 +29,7 @@ public class work1 {
     }
 
     // задание 5
-    static void positivity() {
-        int in = -5;
+    static void positivity(int in) {
         if (in >= 0) {
             System.out.println("Число положительное");
         } else {
@@ -46,8 +38,7 @@ public class work1 {
     }
 
     // задание 6
-    static boolean negativity() {
-        int otr = -2;
+    static boolean negativity(int otr) {
         if (otr < 0) {
             return true;
         } else {
@@ -56,15 +47,13 @@ public class work1 {
     }
 
     // задание 7
-    static void greeting() {
-        String name = "Михаил";
+    static void greeting(String name) {
         System.out.println("Привет, " + name + "!");
     }
 
     // задание 8
-    static void leaping() {
-        double year, q, w, e, res1, res2, res3;
-        year = 2024;
+    static void leaping(int year) {
+        double q, w, e, res1, res2, res3;
         q = 4;
         w = 100;
         e = 400;
