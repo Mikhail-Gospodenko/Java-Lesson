86ce699b541c952bd7e8c4d2e1ef8aabd30a1f0b
AlekseyGospodenko <victim862@gmail.com>
2022-11-06 13:38:58 +0300

    review

--- src/main/java/homework4/practice.java
+++ src/main/java/homework4/practice.java
@@ -1,47 +1,45 @@
 package homework4;
+
 import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.Random;
+
 public class practice {
     public static void main(String[] args) {
-        boolean b;
+        boolean b = false;
 
         Scanner scan = new Scanner(System.in);
-        int[] randomNum = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         do {
-            System.out.println("Угадайте число от 0 до 9");
-            int try1 = scan.nextInt();
             Random rand = new Random();
-            int x = rand.nextInt(9);
-            if (try1 == randomNum[x]) {
-                System.out.println("Вы вы угадали правильное число");
-            } else if (try1 > randomNum[x]) {
-                System.out.println("Число меньше");
-            } else if (try1 < randomNum[x]) {
-                System.out.println("Число больше");
+            int guessNumber = rand.nextInt(9);
+            System.out.println("Угадайте число от 0 до 9");
+
+            boolean isGuess = false;
+            int tryCount = 0;
+
+            while (isGuess != true && tryCount < 3) {
+                int userNumber = scan.nextInt();
+                isGuess = isGuess(userNumber, guessNumber);
+                tryCount++;
             }
-            int try2 = scan.nextInt();
-            if (try2 == randomNum[x]) {
-                System.out.println("Вы вы угадали правильное число");
-            } else if (try2 > randomNum[x]) {
-                System.out.println("Число меньше");
-            } else if (try2 < randomNum[x]) {
-                System.out.println("Число больше");
-            }
-            int try3 = scan.nextInt();
-            if (try3 == randomNum[x]) {
-                System.out.println("Вы вы угадали правильное число");
-            } else if (try3 > randomNum[x]) {
-                System.out.println("Вы не угадали");
-            } else if (try3 < randomNum[x]) {
-                System.out.println("Вы не угадали");
-            }
+
             System.out.println("Попробуйте ещё 1 - да");
             int again = scan.nextInt();
             if (again == 1) {
                 b = true;
             }
-            while (!b) ;
+        } while (b);
+    }
+
+    static boolean isGuess(int userNumber, int guessNumber) {
+        if (userNumber == guessNumber) {
+            System.out.println("Вы угадали правильное число");
+            return true;
+        } else if (userNumber > guessNumber) {
+            System.out.println("Число меньше");
+        } else if (userNumber < guessNumber) {
+            System.out.println("Число больше");
         }
+        return false;
     }
 }
