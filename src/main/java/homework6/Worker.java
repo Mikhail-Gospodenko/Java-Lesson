4838e1b88bcff3fe48d5f1172da2a903e006b506
Mikhail-Gospodenko <mgospodenko@gmail.com>
2022-12-04 11:39:52 +1000

    hw

---
+++ src/main/java/homework6/Worker.java
@@ -0,0 +1,36 @@
+package homework6;
+
+public class Worker {
+    private static String name;
+    private static String post;
+    private static String email;
+    private static String phoneNumber;
+    private static int pay;
+    private static int age;
+
+    public Worker(String name, String post, String email, String phoneNumber, int pay, int age) {
+        this.name = name;
+        this.post = post;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.pay = pay;
+        this.age = age;
+    }
+    public static void printInfo (){
+        System.out.print("ФИО " + name + " должность " + post + " почта " + email + " номер телефона " + phoneNumber + " зарплата " + pay + " возраст " + age);
+    }
+
+    public static void main(String[] args) {
+        Worker[] workerArray = new Worker[4];
+        workerArray[0] = new Worker("Ivanov Ivan", "Engineer", "? ivivan@mailbox.com? ", "892312312", 30000, 30);
+        workerArray[1] = new Worker("Petrov Roman", "Senior Engineer", "? petrovr@mailbox.com? ", "890958312", 50000, 43);
+        workerArray[2] = new Worker("Shzukov Aleksandr", "Electrician", "? shzusan@mailbox.com? ", "892216792", 30000, 27);
+        workerArray[3] = new Worker("Romanov Ivan", "Junior Engineer", "? romanivan@mailbox.com? ", "891108512", 25000, 23);
+        workerArray[4] = new Worker("Dolgov Matvey", "Director", "? dolgov@mailbox.com? ", "898091354", 60000, 50);
+        for (int i=0; i < workerArray.length; i++) {
+            if (workerArray[i].age > 40) {
+                workerArray[i].printInfo();
+            }
+        }
+    }
+}
